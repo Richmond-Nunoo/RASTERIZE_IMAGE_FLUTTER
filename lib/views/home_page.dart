b56b1ddc53252c5d6e0e3ab952d97edb8cf4d3da
Richmond Nunoo <1957richmond@gmail.com>
2023-06-25 12:29:01 +0000

    improving ui,

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image/image.dart' as imglib;
@@ -11,7 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final Color fgColor = Colors.black;
+//  final Color fgColor = Colors.black;
 
   final Color bgColor = Colors.grey.shade200;
 
@@ -26,7 +25,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> loadImage() async {
-    final bytes = await rootBundle.load('assets/images/man.jpg');
+    final bytes = await rootBundle.load('assets/images/woman.jpg');
     final Uint8List imageBytes = bytes.buffer.asUint8List();
     final img = imglib.decodeImage(imageBytes);
     final resizedImage = imglib.copyResize(img!, width: 300, height: 300);
@@ -39,6 +38,22 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
       backgroundColor: bgColor,
       appBar: AppBar(
         title: const Text("Restaurize Image"),
@@ -54,17 +69,19 @@ class _HomePageState extends State<HomePage> {
               width: 300,
               child: LayoutBuilder(
                 builder: (BuildContext context, BoxConstraints constraints) {
-                  const width = 300;
-                  const height = 300;
-                  const ratio = height / width;
+                  final width = constraints.maxWidth;
+                  final height = constraints.maxHeight;
+                  final ratio = height / width;
                   const tilesX = 30;
-                  const tilesY = ratio * tilesX;
-                  tileSize = width / tilesX;
+                  final tilesY = ratio * tilesX;
+                  tileSize = width / tilesY;
                   return GestureDetector(
-                    onPanUpdate: (details) {},
+                    onPanUpdate: (details) {
+                      print(width);
+                    },
                     child: CustomPaint(
                       painter: ImagePainter(
-                        fgColor: Colors.white,
+                        fgColor: bgColor,
                         tileSize: tileSize,
                         image: image,
                       ),
@@ -73,6 +90,13 @@ class _HomePageState extends State<HomePage> {
                 },
               ),
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Download"),
+            )
           ],
         ),
       ),
@@ -97,11 +121,20 @@ class ImagePainter extends CustomPainter {
     for (var y = 0; y < image!.height; y += tileSize.toInt()) {
       for (var x = 0; x < image!.width; x += tileSize.toInt()) {
         final c = image!.getPixelSafe(x, y);
-        final r = c.r;
-        final g = c.g;
-        final b = c.b;
+        //final alpha = Random().nextDouble();
+        final alpha = c.a / 255.0;
+        final r = (c.r * alpha);
+        final g = (c.g * alpha);
+        final b = (c.b * alpha);
+
         final color = Color.fromARGB(255, r.toInt(), g.toInt(), b.toInt());
-        paint.color = color;
+
+        final factor = alpha < 0.5 ? 2.0 : 1.0;
+
+        final blendedColor =
+            Color.alphaBlend(color.withOpacity(alpha * factor), fgColor);
+
+        paint.color = blendedColor.withOpacity(alpha);
         final rect =
             Rect.fromLTWH(x.toDouble(), y.toDouble(), tileSize, tileSize);
         canvas.drawRect(rect, paint);
