d3414a51065a18d604ff364282f9f1da903b9da8
Richmond Nunoo <1957richmond@gmail.com>
2023-07-13 23:47:22 +0000

    work on woke quotes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple
+        ),
       ),
       home: const HomePage(),
     );
